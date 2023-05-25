8939066b3f683600d170c4d51bc03ba7af84c3e5
pooh-14 <poojavetal14@gmail.com>
2023-05-25 21:30:24 +0530

    done

---
+++ Day14-25-05.js
@@ -0,0 +1,17 @@
+// var kuchbhi = 'awdiz';
+// console.log(kuchbhi);
+
+var kuchbhi = '12345';
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
+
+var kuchbhi = 12345;
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
+
+// var booleanValue = 'false';
+// console.log(booleanValue)
+// console.log(typeof (booleanValue))
+// var booleanValue = false;
+// console.log(booleanValue)
+// console.log(typeof (booleanValue))
