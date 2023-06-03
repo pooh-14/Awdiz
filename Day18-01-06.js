6504b691cdcc52cb8a8cd7f2f2424df7b9530c50
pooh-14 <poojavetal14@gmail.com>
2023-06-03 17:00:35 +0530

    done

--- Day18-01-06.js
+++ Day18-01-06.js
@@ -10,23 +10,23 @@
 // }
 
 
-ASSIGNMENT DAY 18
+// ASSIGNMENT DAY 18
 
-var target = 93;
-var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7];
-for(i=0; i<array.length; i++){
-    for(j=1; j<array.length; j++){
-        if(array[i]-array[j]==target){
-            console.log(array[i], array[j], "target achieved")
-        }
-    }
-}
+// var target = 93;
+// var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7];
+// for(i=0; i<array.length; i++){
+//     for(j=1; j<array.length; j++){
+//         if(array[i]-array[j]==target){
+//             console.log(array[i], array[j], "target achieved")
+//         }
+//     }
+// }
 
 var target = 93;
 var array = [3, 65, 32, 6, 23, 56, 76, 17, 100, 7, 90,2,1];
 for(i=0; i<array.length;i++){
-    for(j=1;j<array.length;j++){
-        for(k=1;k<array.length;k++){
+    for(j=i+1;j<array.length;j++){
+        for(k=j+1;k<array.length;k++){
             if(array[i]+array[j]+array[k]==target){
                 console.log(array[i],array[j],array[k],"target achieved")
             }
